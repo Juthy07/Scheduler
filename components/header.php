410861d0c254531118350d363d38eed980451a76
Juthy07 <juthy.shaji@gmail.com>
2021-06-05 17:29:14 +0530

    added header

---
+++ components/header.php
@@ -0,0 +1,11 @@
+<div class="w-full">
+    <div class="flex justify-between border w-full bg-purple-6+00  text-white">
+        <div class="px-4 py-1">
+            <h1 class="text-3xl">Scheduler</h1>
+        </div>
+        <div class="py-4">
+            <span><a href="#" class=" pr-4">Register</a></span>
+            <span><a href="#"  class=" pr-4">Login</a></span>
+        </div>
+    </div>
+</div>
---
+++ pages/dashboard.php
@@ -0,0 +1,13 @@
+<?php 
+    require_once '../components/partials/head.php';
+    require_once '../components/header.php';
+    $head = head('Dashboard');
+
+?>
+
+<html>
+    <?php echo $head ?>
+    <body>
+        <?php require_once '../components/header.php';?>
+    </body>
+</html>
